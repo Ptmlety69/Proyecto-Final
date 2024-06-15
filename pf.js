821a36be1dd8b51fd743b5ffa9cb9c9c979a78cb
Ptmlety69 <ptmlety@gmail.com>
2024-06-15 16:31:41 -0600

    estructura lista falta CSS

---
+++ pf.js
@@ -0,0 +1,6 @@
+
+function cambiarinvierno() {
+    document.body.style.backgroundImage = "url('invierno.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundRepeat = "no-repeat";
+}
