f02f10adf1fd9c2de6f2da71a6aae9756a8dcaab
Blueish-Candy <nuevojuannn@outlook.com>
2024-06-16 00:08:56 -0600

    Update estilos.css

--- pf.js
+++ pf.js
@@ -1,6 +1,11 @@
 
-function cambiarinvierno() {
+function invierno() {
     document.body.style.backgroundImage = "url('invierno.jpg')";
     document.body.style.backgroundSize = "cover";
     document.body.style.backgroundRepeat = "no-repeat";
 }
+let user=document.getElementById('usuario')
+let contra= document.getElementById('contraseña')
+let IS=document.getElementById('IS')
+let prod= document.getElementById('productos')
+
