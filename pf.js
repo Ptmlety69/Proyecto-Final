b576f1d71da46228a1bed534735efe2b10c8e1d7
Blueish-Candy <nuevojuannn@outlook.com>
2024-06-16 22:13:06 -0600

    Update estilos.css

--- pf.js
+++ pf.js
@@ -1,6 +1,25 @@
-
+function inicio() {
+    document.body.style.backgroundImage = "url('fondo1.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundRepeat = "no-repeat";
+}
 function invierno() {
     document.body.style.backgroundImage = "url('invierno.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundRepeat = "no-repeat";
+}
+function primavera() {
+    document.body.style.backgroundImage = "url('primavera.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundRepeat = "no-repeat";
+}
+function otoño() {
+    document.body.style.backgroundImage = "url('otoño.jpg')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundRepeat = "no-repeat";
+}
+function verano() {
+    document.body.style.backgroundImage = "url('verano.jpg')";
     document.body.style.backgroundSize = "cover";
     document.body.style.backgroundRepeat = "no-repeat";
 }
@@ -8,4 +27,3 @@ let user=document.getElementById('usuario')
 let contra= document.getElementById('contraseña')
 let IS=document.getElementById('IS')
 let prod= document.getElementById('productos')
-
