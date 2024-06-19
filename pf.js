50fdf65e837c2cd7c95d288d70902c67927b35d9
Blueish-Candy <nuevojuannn@outlook.com>
2024-06-18 20:46:44 -0600

    Actualizacion 2
    Falta estilos de la tabla y poquito JS para los botones

--- pf.js
+++ pf.js
@@ -26,4 +26,134 @@ function verano() {
 let user=document.getElementById('usuario')
 let contra= document.getElementById('contraseña')
 let IS=document.getElementById('IS')
-let prod= document.getElementById('productos')
+let frutas= document.getElementById('productos')
+let datos= document.getElementById('registrotabla')
+let cant=document.getElementById('cant')
+let prec=document.getElementById('prec')
+let cinco=document.getElementById('cinco')
+let diez=document.getElementById('diez')
+let quince=document.getElementById('quince')
+let botonG=document.getElementById('Guardar')
+let botonV=document.getElementById('Vaciar')
+
+
+
+
+
+let productos = []
+
+document.addEventListener("DOMContentLoaded", carga)
+
+function carga() {
+    if (localStorage.getItem("productos") === null) {
+        productos = []
+    } else {
+        productos = JSON.parse(localStorage.getItem("productos"))
+    }
+
+    let salida = ""
+
+    productos.forEach((prod, indice) => {
+        salida += `<tr>
+                    <td>${prod.fruta}</td>
+                    <td>${prod.img}</td>
+                    <td>${prod.cant}</td>
+                    <td>${prod.prec}</td>
+                    <td>${prod.subt}</td>
+                    <td>${prod.imp}</td>
+                    <td>${prod.tot}</td>
+                    <td> <input type="button" value="Eliminar"  id="elim" onclick="eliminar(${indice})">
+                    <input type="button" value="Modificar" onclick="modificar(${indice})"> </td>
+                </tr>`
+        
+    });
+
+                    
+
+    datos.innerHTML = salida
+}
+
+// botonA.addEventListener("click",()=>{
+//     botones.style.display = "flex"
+//     botones2.style.display = "none"
+//     productos[indice].nombre=nombre.value
+//     productos[indice].rfc=rfc.value
+//     productos[indice].pues=pues.value
+//     productos[indice].suel=suel.value
+//     productos[indice].dt=dt.value
+//     productos[indice].imp=imp.value
+//     localStorage.setItem("productos",JSON.stringify(productos))
+//     carga()
+// })
+
+// function quitar(i) {
+//     botones.style.display = "none"
+//     botones2.style.display = "flex"
+//     let confirma = confirm("¿Estás seguro(a)(e) de que quieres Eliminar el alumno "+productos[i].nombre+"?")
+//     if (confirma) {
+//         productos.splice(i,1)
+//         localStorage.setItem("productos",JSON.stringify(productos))
+        
+//         carga()
+//     }
+// }
+
+botonG.addEventListener("click", () => {
+    if(cinco.checked){
+            impu=0.05
+            }else if(diez.checked){
+                impu=0.10
+            }else{
+                impu=0.15
+            }
+    let prod = {
+        fruta: frutas.value,
+        img: "pongan imagen porfaa",
+        cant: cant.value,
+        prec: prec.value,
+        subt: (parseFloat(cant.value) * parseFloat(prec.value)),
+        imp: (parseFloat(cant.value) * parseFloat(prec.value))*impu,
+        tot: (parseFloat(cant.value) * parseFloat(prec.value))-(impu*(parseFloat(cant.value) * parseFloat(prec.value)))
+    }
+    productos.push(prod)
+
+    localStorage.setItem("productos", JSON.stringify(productos))
+    carga()
+})
+
+botonV.addEventListener("click", () => {
+    let confirma = confirm("¿Estás seguro de que quieres vaciar la tabla?")
+    if (confirma) {
+        localStorage.clear()
+        carga()
+    }
+});
+
+// botonC.addEventListener("click", () => {
+//     let a = prompt("Nombre del trabajador a Eliminar")
+//     if (productos.find(alu => alu.nombre === a)) {
+//         productos = productos.filter(al => al.nombre != a);
+//         localStorage.setItem("productos", JSON.stringify(productos))
+//         carga()
+//     } else {
+//         alert("trabajador no existe")
+//     }
+// });
+
+// function eliminar(indice) {
+//     productos.splice(indice, 1)
+//     localStorage.setItem("productos", JSON.stringify(productos))
+//     carga()
+// }
+
+// function modificar(i) {
+//     indice=1
+//     nombre.value=productos[i].nombre
+//     rfc.value=productos[i].rfc
+//     pues.value=productos[i].pues
+//     suel.value=productos[i].suel
+//     dt.value=productos[i].dt
+//     imp.value=productos[i].imp
+//     botones.style.display = "none"
+//     botones2.style.display = "flex"
+// }
